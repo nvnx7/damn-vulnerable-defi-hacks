b8399b76200b2e57c802ac1af7c857a85b7fe8b0
theNvN <thenvnsahu@gmail.com>
2022-03-14 20:27:47 +0530

    comments

--- contracts/side-entrance/SideEntranceLenderPool.sol
+++ contracts/side-entrance/SideEntranceLenderPool.sol
@@ -14,12 +14,23 @@ interface IFlashLoanEtherReceiver {
 contract SideEntranceLenderPool {
     using Address for address payable;
 
-    mapping (address => uint256) private balances;
+    mapping(address => uint256) private balances;
 
+    /**
+     *
+     * 凸 ( ͡◣ ͜ʖ ͡◢)凸
+     * VULNERABILITY: Can be called in receiver's execute function
+     * to deposit the loan amount back into pool
+     */
     function deposit() external payable {
         balances[msg.sender] += msg.value;
     }
 
+    /**
+     *
+     * (╯ ͡ಠ ͜ʖ ͡ಠ)╯┻━┻
+     * AFFECTED
+     */
     function withdraw() external {
         uint256 amountToWithdraw = balances[msg.sender];
         balances[msg.sender] = 0;
@@ -29,10 +40,17 @@ contract SideEntranceLenderPool {
     function flashLoan(uint256 amount) external {
         uint256 balanceBefore = address(this).balance;
         require(balanceBefore >= amount, "Not enough ETH in balance");
-        
+
+        /**
+         *
+         * 凸 ( ͡◣ ͜ʖ ͡◢)凸
+         * VULNERABILITY: External contract function called
+         */
         IFlashLoanEtherReceiver(msg.sender).execute{value: amount}();
 
-        require(address(this).balance >= balanceBefore, "Flash loan hasn't been paid back");        
+        require(
+            address(this).balance >= balanceBefore,
+            "Flash loan hasn't been paid back"
+        );
     }
 }
- 
